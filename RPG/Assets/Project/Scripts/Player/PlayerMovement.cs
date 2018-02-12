62d9a910ea10d71682d996e11823f58c3e239649
ANAS-PC\AnasIqbal <anas.iqbal@binexsolutions.com>
2018-02-12 20:58:24 +0500

    Initial Commit

---
+++ RPG/Assets/Project/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent (typeof (Animator))]
+public class PlayerMovement : MonoBehaviour
+{
+	Animator anim;
+
+	void Awake()
+	{
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update()
+	{
+		Move ();
+	}
+
+	void Move()
+	{
+		anim.SetFloat ("Forward", Input.GetAxisRaw("Vertical"));
+	}
+
+}
